415d397ceb3daad14f3df881610bfc795598ef43
Kevin Patel <kpatel1209@gmail.com>
2019-02-24 16:55:34 -0500

    db, config, model, and controller set up initiated

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+const connection = require("../config/connection");
+
+let orm = {
+    selectAll: function() {
+
+    },
+
+    insertOne: function() {
+
+    },
+
+    updateOne: function() {
+
+    }
+
+};
+
+// Export the orm object for the model
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Myburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Yourburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hisburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Herburger", true);
