973fc0d3510ba08f7748ab6665d139f6e03d329e
Kevin Patel <kpatel1209@gmail.com>
2019-03-01 10:25:00 -0500

    functionality appears to work, needs css now

--- config/orm.js
+++ config/orm.js
@@ -7,11 +7,19 @@ let orm = {
             cb(result);
         });
     },
+    
     update: function(tableInput, condition, cb) {
         connection.query("UPDATE " +tableInput+ " SET devoured=true WHERE id=" +condition+ ";", function(err, result) {
             if(err) throw error;
             cb(result);
         });
+    },
+
+    create: function(tableInput, val, cb) {
+        connection.query("INSERT INTO " +tableInput+ "(burger_name) VALUES ('"+val+"');", function(err, result) {
+            if(err) throw error;
+            cb(result);
+        })
     }
 }
 
