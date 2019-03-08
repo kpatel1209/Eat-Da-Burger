f8cf1982d497d48c03238d22bfa99688cb9bd77a
Kevin Patel <kpatel1209@gmail.com>
2019-03-08 12:21:08 -0500

    updates

--- config/orm.js
+++ config/orm.js
@@ -1,30 +1,30 @@
 const connection = require("./connection");
 
 let orm = {
-    all: function(tableInput, cb) {
+    all: function(tableInput, callback) {
         connection.query("SELECT * FROM " + tableInput + ";", function(err, result) {
             if(err) {
                 throw err;
             }
-            cb(result);
+            callback(result);
         });
     },
 
-    create: function(tableInput, val, cb) {
-        connection.query("INSERT INTO " + tableInput + "(burger_name) VALUES ('"+val+"');", function(err, result) {
+    create: function(tableInput, value, callback) {
+        connection.query("INSERT INTO " + tableInput + "(burger_name) VALUES ('"+value+"');", function(err, result) {
             if(err) {
                 throw error;
             }
-            cb(result);
+            callback(result);
         });
     },
     
-    update: function(tableInput, condition, cb) {
-        connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function(err, result) {
+    update: function(tableInput, value, callback) {
+        connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + value + ";", function(err, result) {
             if(err) {
                 throw error;
             }
-            cb(result);
+            callback(result);
         });
     }    
 }
